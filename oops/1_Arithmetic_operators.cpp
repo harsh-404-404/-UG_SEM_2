18b6ae5d22db55915330ae0f9dd30519fd36b303
harsh-404-404 <harshbhalala404@gmail.com>
2026-02-06 20:45:02 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Generative AI/simple_RNN/rnn.ipynb
    	modified:   oops/1_Arithmetic_operators.cpp
    	modified:   oops/1_Arithmetic_operators.exe

--- oops/1_Arithmetic_operators.cpp
+++ oops/1_Arithmetic_operators.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
     int x,y,add = 0;
-    cout<< "Enter the value of x & y";
-    cin>> x >> y;
+    cout << "Enter the value of x & y";
+    cin >> x >> y;
     add = x + y;
     cout << "this sum of x and y is: " << add << endl;
     return 0;  
