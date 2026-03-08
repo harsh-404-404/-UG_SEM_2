5236164635d849753396a2e7a953f8973e335821
harsh-404-404 <harshbhalala404@gmail.com>
2026-03-08 21:34:24 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   oop/6_fibonacci_recursion.cpp
    	renamed:    oop/10_atm_demo.cpp -> oop/7_atm_demo.cpp
    	deleted:    oop/7_into_class.cpp
    	deleted:    oop/8_class_constructor.cpp
    	renamed:    oop/11_dynemic_array.cpp -> oop/8_dynemic_array.cpp
    	deleted:    oop/9_Encapsulation.cpp
    	renamed:    oop/12_linked_list.cpp -> oop/9_linked_list.cpp

--- oop/6_fibonacci_recursion.cpp
+++ oop/6_fibonacci_recursion.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
+#include <vector>
 
-void printfibo(size_t n, size_t first,size_t secound){
-
-    if (n == 0){
-        return;
-    }
-    size_t third { first + secound };
-    std::cout << third << " ";
-    printfibo(n-1,secound,third);  
-
+int fibonacci(std::size_t i){
+	static std::vector<int> storage { 0,1 };
+	if(i < storage.size()){
+		return storage[i];
+	}
+	storage.push_back(fibonacci(i-1) + fibonacci(i-2));
+	return storage.back();
 }
-
-void fibonacci(size_t n){
-
-    if (n < 3){
-        std::cout << "Minimum number of terms is 3!!";
-    }
-    else{
-        std::cout << "Febonacci Series" << '\n';
-        std::cout << "0 1 ";
-        printfibo(n - 2, 0 , 1);
-    }
-
-}
-
 
 int main(){
-
-    size_t n{};
-    std::cout << "Enter number of terms: ";
-    std::cin >> n;
-    
-    fibonacci(n);
-
+	
+	for(int i = 0; i < 12; ++i){
+		std::cout << fibonacci(i) << ' ';
+	}
 }
